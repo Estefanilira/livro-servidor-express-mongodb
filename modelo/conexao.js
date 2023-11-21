822bb01a2c2fdc75ec033e8be5b2cf59703ca2de
Estefani Lira <estefaniwanderley.lira@gmail.com>
2023-11-21 16:13:49 -0300

    up

---
+++ modelo/conexao.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+};
+
+mongoose.connect('mongodb+srv://admin:admin@livraria.c882jui.mongodb.net/', options);
+
+module.exports = mongoose;
